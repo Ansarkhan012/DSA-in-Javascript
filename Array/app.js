4958a20b3cb560dfd5861b39618fcf92233ee041
Ansarkhan012 <ansarkhanaptech1234@gmail.com>
2025-09-29 04:36:14 -0700

    Array update

---
+++ Array/app.js
@@ -0,0 +1,30 @@
+// let arr = [1,2 ,3,4,5 , "Ansar"]
+// console.log(arr)
+// console.log(arr[5])
+
+// let arr = []
+// arr[0] = 10
+// arr[1] = 20
+// arr[2] = 30
+// arr[3] = 40
+
+// arr[7] = 90
+
+// console.log(arr)
+
+// Array hamesha dynamic hogi
+
+// let arr = new Array(3);
+// arr[0] = 10
+// arr[1] = 20
+// arr[2] = 30
+// arr[3] = 40
+// arr[10] = 140
+
+// console.log(arr)
+
+
+let arr = new Array(2)
+
+arr.push(45)
+console.log(arr)
--- Pattern/App.js
+++ Pattern/App.js
@@ -3,5 +3,31 @@
 
 
 let prompt = require("prompt-sync")();
-let a = prompt("Enter a number :");
-process.stdout.write(a);
+let n = Number(prompt("Enter a number :"));
+
+
+
+// simple pattern
+// for(let j = 1; j <= n ; j++){
+//     for(let i = 1; i <= n ; i++){
+//     process.stdout.write("* ")
+// }
+// console.log();
+// }
+
+
+
+// for(let i=1; i<=n;i++){
+//     for(let j=1;j<=i;j++){
+//         process.stdout.write("* ")
+//     }
+//     console.log()
+// }
+
+
+for(let i=1; i<=n;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(j +" ")
+    }
+    console.log()
+}
